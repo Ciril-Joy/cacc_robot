0307d34fe20eb0d1f61cbe71b751d6c58f696d17
Ciril-Joy <ciriljoy@karunya.edu.in>
2025-05-06 00:42:22 +0530

    its 1AM nad idk what i did or doing

---
+++ cacc_description/launch/view_robot.launch.py
@@ -0,0 +1,44 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    robot_description_path = PathJoinSubstitution([
+        FindPackageShare('cacc_description'),
+        'urdf',
+        'robot.urdf.xacro'
+    ])
+
+    robot_description_content = Command(['xacro ', robot_description_path])
+
+    rviz_config_path = PathJoinSubstitution([
+        FindPackageShare('cacc_description'),
+        'rviz',
+        'robot_view.rviz'
+    ])
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{
+                'robot_description': robot_description_content
+            }]
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            output='screen'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_path],
+            output='screen'
+        )
+    ])
